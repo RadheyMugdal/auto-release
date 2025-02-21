7b644311385d9e767359ac06b6fc6715acfd983a
Radhey Mugdal <mugdalradhey@gmail.com>
2025-02-21 16:54:38 +0530

    patch: changes without changelog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ export default function Home() {
   return (
     <div className=" w-screen h-screen bg-black flex items-center justify-center ">
       <h1 className=" text-white text-2xl font-bold">Landing page</h1>
+      <div>
+        <button type="button" className=" bg-white p-3 rounded-md text-black">
+          Click here
+        </button>
+      </div>
+      <p>Changes without changelog</p>
     </div>
   );
 }
